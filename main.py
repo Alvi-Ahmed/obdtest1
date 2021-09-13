e3c3665f24de9999eff2c2ee46867e4fb7b61334
Alvi-Ahmed <alviahmed12@gmail.com>
2021-09-13 16:12:38 +0600

    and

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import obd
+import time
 
 # connection = obd.OBD(protocol="7", baudrate="9600", fast=False)
 connection = obd.OBD(baudrate=38400, fast=False)
@@ -7,8 +8,12 @@ print(r)
 # connection = obd.OBD() # auto-connects to USB or RF port
 
 cmd = obd.commands.SPEED # select an OBD command (sensor)
+cmd2 = obd.commands.COOLANT_TEMP # select an OBD command (sensor)
+response2 = connection.query(cmd2) # send the command, and parse the response
+print(response2.value)
 
 response = connection.query(cmd) # send the command, and parse the response
 
 print(response.value) # returns unit-bearing values thanks to Pint
 print(response.value.to("mph")) # user-friendly unit conversions
+time.sleep(0.5)
