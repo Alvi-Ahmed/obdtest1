7abb9296e3b7e227e3ffc29c98d2d00d5d821ff9
Alvi-Ahmed <alviahmed12@gmail.com>
2021-11-01 02:39:18 +0600

    obd data found

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import obd
 import time
 
 # connection = obd.OBD(protocol="7", baudrate="9600", fast=False)
-connection = obd.OBD(baudrate=38400, fast=False)
+#connection = obd.OBD(baudrate=38400, fast=False)
+#connection = obd.OBD() # auto-connects to USB or RF port
+connection = obd.OBD("/dev/pts/1")
+
+
 r= connection.status()
 print(r)
-# connection = obd.OBD() # auto-connects to USB or RF port
-
-cmd = obd.commands.SPEED # select an OBD command (sensor)
-cmd2 = obd.commands.COOLANT_TEMP # select an OBD command (sensor)
-response2 = connection.query(cmd2) # send the command, and parse the response
-print(response2.value)
-
-response = connection.query(cmd) # send the command, and parse the response
-
-print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("mph")) # user-friendly unit conversions
-time.sleep(0.5)
+a=0
+while (a<1):
+    cmd = obd.commands.SPEED # select an OBD command (sensor)
+    cmd3 = obd.commands.RPM
+    response3 = connection.query(cmd3) # send the command, and parse the response
+    print(response3.value)
+    cmd2 = obd.commands.COOLANT_TEMP # select an OBD command (sensor)
+    response2 = connection.query(cmd2) # send the command, and parse the response
+    print(response2.value)
+    response = connection.query(cmd) # send the command, and parse the response
+    print(response.value) # returns unit-bearing values thanks to Pint
+#time.sleep(0.5)
