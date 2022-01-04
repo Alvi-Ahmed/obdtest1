b0d4bb47e0ac1bff0de30285ca070045cdf56eaf
Alvi-Ahmed <alviahmed12@gmail.com>
2022-01-05 03:32:56 +0600

    new

--- maintwo.py
+++ maintwo.py
@@ -33,8 +33,10 @@ r= connection.status()
 print(r)
 a=0
 i=1
-infdriving = False
-reckless= True
+n=0
+infdriving = 0
+reckless= 0
+overspeed =0
 # with open('data.csv','a',newline='') as csvfile:
 #     fieldnames = ['speed','coolent']
 #    # writer = csv.DictWrite(csvfile)
@@ -44,7 +46,23 @@ with open('data.csv','a',newline='') as csvfile:
         pitter = csv.DictWriter(csvfile, fieldnames=["a","b","c"])
         pitter.writeheader()
         csvfile.close
-while (a<100):
+with open('topspeed.csv','a',newline='') as csvfile:
+        pitter = csv.DictWriter(csvfile, fieldnames=["date","topspeed"])
+        pitter.writeheader()
+        csvfile.close 
+with open('reckless.csv','a',newline='') as csvfile:
+        pitter = csv.DictWriter(csvfile, fieldnames=["date","reckless"])
+        pitter.writeheader()
+        csvfile.close
+with open('ineffecient.csv','a',newline='') as csvfile:
+        pitter = csv.DictWriter(csvfile, fieldnames=["date","inefficient"])
+        pitter.writeheader()
+        csvfile.close    
+with open('overspeed.csv','a',newline='') as csvfile:
+        pitter = csv.DictWriter(csvfile, fieldnames=["date","overspeed"])
+        pitter.writeheader()
+        csvfile.close              
+while (a<2000):
     cmd = obd.commands.SPEED # select an OBD command (sensor)
     cmd3 = obd.commands.RPM
     cmd4=obd.commands.THROTTLE_ACTUATOR
@@ -56,10 +74,10 @@ while (a<100):
     y2=float(y1[0])
     if y2>3000:
         print('fuel inefficient Driving')
-        infdriving=True
+        infdriving=infdriving+1
     if y2>4000:
         print('Recless Driving')
-        reckless=True
+        reckless=reckless+1
     print(y)
     cmd2 = obd.commands.COOLANT_TEMP # select an OBD command (sensor)
     response2 = connection.query(cmd2) # send the command, and parse the response
@@ -68,24 +86,66 @@ while (a<100):
     ref=db.reference('py/')
     now=datetime.now()
 
-    dt_string = now.strftime('%d-%m-%Y %H-%M-%S')
+    dt_string = round(time.time()*1000)
 # dtime=str(today)
     print(dt_string)
 
     #time.sleep(0.5)
     x = str(response.value)
-    print(x)
+    x1 = x.split(" ",1)
+    x2 = float(x1[0])
+    print(x2)
+    if(x2>n):
+        n=x2
+    if(x2>110.0):
+        overspeed=overspeed+1    
     a=a+1
     with open('data.csv','a',newline='') as csvfile:
         # pitter = csv.DictWriter(csvfile, fieldnames=["a","b","c"])
         # pitter.writeheader()
         writer = csv.writer(csvfile)
        # writer.writeheader()
-        data=[dt_string,response,response2]
+        data=[dt_string,x2,response2]
         writer.writerow(data)
         csvfile.close() 
 
- 
+print(n)
+dt_mili = round(time.time()*1000)
+with open('topspeed.csv','a',newline='') as csvfile:
+        # pitter = csv.DictWriter(csvfile, fieldnames=["a","b","c"])
+        # pitter.writeheader()
+        writer = csv.writer(csvfile)
+       # writer.writeheader()
+        data=[dt_mili,n]
+        writer.writerow(data)
+        csvfile.close() 
+print(reckless)
+with open('reckless.csv','a',newline='') as csvfile:
+        # pitter = csv.DictWriter(csvfile, fieldnames=["a","b","c"])
+        # pitter.writeheader()
+        writer = csv.writer(csvfile)
+       # writer.writeheader()
+        data=[dt_mili,reckless]
+        writer.writerow(data)
+        csvfile.close() 
+print(infdriving)
+with open('ineffecient.csv','a',newline='') as csvfile:
+        # pitter = csv.DictWriter(csvfile, fieldnames=["a","b","c"])
+        # pitter.writeheader()
+        writer = csv.writer(csvfile)
+       # writer.writeheader()
+        data=[dt_mili,infdriving]
+        writer.writerow(data)
+        csvfile.close()  
+print(overspeed)
+with open('overspeed.csv','a',newline='') as csvfile:
+        # pitter = csv.DictWriter(csvfile, fieldnames=["a","b","c"])
+        # pitter.writeheader()
+        writer = csv.writer(csvfile)
+       # writer.writeheader()
+        data=[dt_mili,overspeed]
+        writer.writerow(data)
+        csvfile.close() 
 
 data_ref.set({
     'Reckless': reckless,
